ca840232efac3a8cfa549c51e5f99030654db2c7
yeongjae0113 <dudwo0113@gmail.com>
2024-07-16 17:34:46 +0900

    UserServiceImpl (1) 2024-07-16

--- Bt62_JPA_Board/src/main/java/com/lec/spring/repository/AuthorityRepository.java
+++ Bt62_JPA_Board/src/main/java/com/lec/spring/repository/AuthorityRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-
+    Authority findByName(String name);
 }
 
 
--- Bt62_JPA_Board/src/main/java/com/lec/spring/service/UserServiceImpl.java
+++ Bt62_JPA_Board/src/main/java/com/lec/spring/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,24 +20,48 @@ public class UserServiceImpl implements UserService {
     private AuthorityRepository authorityRepository;
 
 
+
+    @Autowired
+    public void userRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Autowired
+    public void authorityRepository(AuthorityRepository authorityRepository) {
+        this.authorityRepository = authorityRepository;
+    }
+
     @Override
     public User findByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username.toUpperCase());
     }
 
     @Override
     public boolean isExist(String username) {
-        return false;
+        User user = findByUsername(username);
+        return user != null;    // user 가 null 인지 아닌지 확인하는 논리식 (반환타입은 boolean)
     }
 
     @Override
     public int register(User user) {
-        return 0;
+        user.setUsername(user.getUsername().toUpperCase());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user = userRepository.save(user);
+
+        // 신규회원 ROLE_MEMBER 권한 부여
+        Authority auth = authorityRepository.findByName("ROLE_MEMBER");
+        user.addAuthority(auth);
+        userRepository.save(user);
+
+        return 1;
     }
 
     @Override
     public List<Authority> selectAuthoritiesById(Long id) {
-        return null;
+        User user = userRepository.findById(id).orElse(null);
+        if(user != null)
+            return user.getAuthorities();
+
+        return new ArrayList<>();
     }
 }
 
